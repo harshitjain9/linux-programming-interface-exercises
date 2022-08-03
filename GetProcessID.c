8142a449e255bac9d42213a7155785802d049f18
harshit <harsheetjain>
2022-08-03 11:54:20 +0800

    add processes

---
+++ GetProcessID.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    int proc_id, par_proc_id;
+    proc_id = getpid();
+    par_proc_id = getppid();
+
+    printf("PROCESS ID: %d\n", proc_id);
+    printf("PARENR PROCESS ID: %d\n", par_proc_id);
+}
